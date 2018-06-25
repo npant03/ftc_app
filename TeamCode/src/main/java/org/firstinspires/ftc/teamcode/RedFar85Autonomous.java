961d58896b2a334de608eb78abb9624ee6e7975d
npant03 <npant03@gmail.com>
2018-06-25 09:48:31 -0700

    added red far auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFar85Autonomous.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by nehapant on 6/24/18.
+ */
+
+@Autonomous(name="RedFar85Autonomous", group="Team5214")
+
+public class RedFar85Autonomous extends RedAutonomous{
+
+    @Override
+    protected void getToLeftDumpPosition(){
+        straightWithEncoder(.5, -24);
+        leftDump.setPosition(.61);
+        strafeWithEncoder(.5, 4);
+        turnLeftDegrees(38, parameters);
+    }
+
+
+    @Override
+    protected void getToCenterDumpPosition(){
+        straightWithEncoder(.55, -24);
+        leftDump.setPosition(.61);
+        turnLeftDegrees(35, parameters);
+    }
+
+    @Override
+    protected void getToRightDumpPosition(){
+        straightWithEncoder(.5, -24);
+        leftDump.setPosition(.61);
+        turnLeftDegrees(12, parameters);
+    }
+
+    @Override
+    protected void getToDefaultDumpPosition(){
+        getToLeftDumpPosition();
+    }
+
+
+}
