ada7e029cdff0406baa7180e36e131e9b3a82993
npant03 <npant03@gmail.com>
2018-09-04 14:47:04 -0700

    color sensor class
    it's not working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryAutonomous.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
